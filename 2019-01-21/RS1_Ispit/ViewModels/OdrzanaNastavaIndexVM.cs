2d27455343b6290fef1445b71b1f3aab7613cf36
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-16 21:36:00 +0200

    Create OdrzanaNastavaIndexVM.cs

---
+++ 2019-01-21/RS1_Ispit/ViewModels/OdrzanaNastavaIndexVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class OdrzanaNastavaIndexVM
+    {
+        public int NastavnikId { get; set; }
+        public string Skola { get; set; }
+        public string Nastavnik { get; set; }
+    }
+}
