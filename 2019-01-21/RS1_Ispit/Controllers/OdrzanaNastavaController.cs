e4d9f53b9b68cf7fc50124d77b7da8ead39076a1
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-16 22:02:12 +0200

    Update OdrzanaNastavaController.cs

--- 2019-01-21/RS1_Ispit/Controllers/OdrzanaNastavaController.cs
+++ 2019-01-21/RS1_Ispit/Controllers/OdrzanaNastavaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RS1_Ispit_asp.net_core.EF;
 using RS1_Ispit_asp.net_core.EntityModels;
 using RS1_Ispit_asp.net_core.ViewModels;
@@ -24,6 +25,35 @@ namespace RS1_Ispit_asp.net_core.Controllers
                 Skola=db.PredajePredmet.Where(y=>y.NastavnikID==x.Id).Select(y=>y.Odjeljenje.Skola.Naziv).FirstOrDefault(),
                 Nastavnik=x.Ime+" "+x.Prezime
             }).ToList();
+            return View(model);
+        }
+
+        public IActionResult Prikaz(int NastavnikId)
+        {
+            PrikazVM model = new PrikazVM
+            {
+                NastavnikId = NastavnikId,
+                Rows = db.MaturskiIspit.Where(x => x.NastavnikId == NastavnikId).Select(x => new PrikazVM.Row
+                {
+                    Datum = x.Datum.ToShortDateString(),
+                    Škola = x.Skola.Naziv,
+                    Predmet = x.Predmet.Naziv,
+                    MaturskiIspitId = x.Id,
+
+
+                }).ToList(),
+            };
+            foreach(var z in model.Rows)
+            {
+                var odsutni = db.MaturskiIspitStavka.Include(x => x.OdjeljenjeStavka).Include(x => x.OdjeljenjeStavka.Ucenik)
+                    .Where(x => x.MaturskiIspitId == z.MaturskiIspitId && !x.IsPristupio).ToList();
+
+                z.NisuPristupili = new List<string>();
+                foreach(var t in odsutni)
+                {
+                    z.NisuPristupili.Add(t.OdjeljenjeStavka.Ucenik.ImePrezime);
+                }
+            }
             return View(model);
         }
     }
