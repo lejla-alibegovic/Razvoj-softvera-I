147d5b9af26b592c298b6a7310472fc407ce6ae5
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-16 23:06:27 +0200

    Create DodajVM.cs

---
+++ 2019-01-21/RS1_Ispit/ViewModels/DodajVM.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class DodajVM
+    {
+        public int MaturskiIspitId { get; set; }
+        public int SkolaId { get; set; }
+        public List<SelectListItem> Skole { get; set; }
+        public int NastavnikId { get; set; }
+        public string Nastavnik { get; set; }
+       
+        public string SkolskaGodina { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Datum { get; set; }
+        public int PredmetId { get; set; }
+        public List<SelectListItem> Predmeti { get; set; }
+    }
+}
