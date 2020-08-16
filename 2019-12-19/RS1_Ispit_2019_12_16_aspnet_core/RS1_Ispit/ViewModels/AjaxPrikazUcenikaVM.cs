0fa43adb3281b74d5e6de963fff80300e3bee867
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-16 16:26:24 +0200

    Create AjaxPrikazUcenikaVM.cs

---
+++ 2019-12-19/RS1_Ispit_2019_12_16_aspnet_core/RS1_Ispit/ViewModels/AjaxPrikazUcenikaVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class AjaxPrikazUcenikaVM
+    {
+        public int PopravniIspitStavkaId { get; set; }
+        public string Ucenik { get; set; }
+        public string Odjeljenje { get; set; }
+        public int BrojUDnevniku { get; set; }
+        public bool IsPrisutan { get; set; }
+        public float? Bodovi { get; set; }
+        public bool MozePristupit { get; set; }
+    }
+}
