79fcc557e504fd929c92cc53b786dc0c3156a6ec
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-17 23:58:21 +0200

    Create UrediVM.cs

---
+++ 2019-01-21/RS1_Ispit/ViewModels/UrediVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class UrediVM
+    {
+        public int MaturskiIspitId { get; set; }
+        public string Napomena { get; set; }
+        public string Datum { get; set; }
+        public string Predmet { get; set; }
+    }
+}
