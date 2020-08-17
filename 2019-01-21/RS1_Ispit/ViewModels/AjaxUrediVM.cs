c8943bfbc870119ef2b168d267871a81d237401b
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-17 23:58:01 +0200

    Create AjaxUrediVM.cs

---
+++ 2019-01-21/RS1_Ispit/ViewModels/AjaxUrediVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class AjaxUrediVM
+    {
+        public int MaturskiIspitStavkaId { get; set; }
+        public int BrojBodova { get; set; }
+        public string Ucenik { get; set; }
+    }
+}
