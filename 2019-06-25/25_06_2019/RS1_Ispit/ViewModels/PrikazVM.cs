fc7d72fb91eab5a6d4de904907487d2f4a044926
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-21 18:44:42 +0200

    Create PrikazVM.cs

---
+++ 2019-06-25/25_06_2019/RS1_Ispit/ViewModels/PrikazVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.ViewModels
+{
+    public class PrikazVM
+    {
+        public string Predmet { get; set; }
+        public string NastavnikImePrezime { get; set; }
+        public string AkademskaGodian { get; set; }
+        public int AngazovanId { get; set; }
+        public IEnumerable<Row> rows { get; set; }
+        public class Row
+        {
+            public string Datum { get; set; }
+            public int BrojStutenataNisuPolozili { get; set; }
+            public int BrojPrijavljenih { get; set; }
+            public bool IsEvidentirano { get; set; }
+            public int IspitId { get; set; }
+        }
+    }
+}
