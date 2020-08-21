513ca6933a567f0a2cdeb6b58718afe70d7ec06d
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-21 14:58:45 +0200

    init

---
+++ 2019-06-25/25_06_2019/RS1_Ispit/EntityModels/Ispit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class Ispit
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(AngazovanId))]
+        public Angazovan Angazovan { get; set; }
+        public int AngazovanId { get; set; }
+        public DateTime Datum { get; set; }
+        public string Napomena { get; set; }
+        public bool IsZakljucano { get; set; }
+    }
+}
---
+++ 2019-06-25/25_06_2019/RS1_Ispit/Migrations/20200821125510_2.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RS1_Ispit_asp.net_core.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ispit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AngazovanId = table.Column<int>(nullable: false),
+                    Datum = table.Column<DateTime>(nullable: false),
+                    IsZakljucano = table.Column<bool>(nullable: false),
+                    Napomena = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ispit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ispit_Angazovan_AngazovanId",
+                        column: x => x.AngazovanId,
+                        principalTable: "Angazovan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IspitStavka",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsPolozio = table.Column<bool>(nullable: false),
+                    IsPrijavio = table.Column<bool>(nullable: false),
+                    IsPristupio = table.Column<bool>(nullable: false),
+                    IspitId = table.Column<int>(nullable: false),
+                    Ocjena = table.Column<int>(nullable: false),
+                    SlusaPredmetId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IspitStavka", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IspitStavka_Ispit_IspitId",
+                        column: x => x.IspitId,
+                        principalTable: "Ispit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_IspitStavka_SlusaPredmet_SlusaPredmetId",
+                        column: x => x.SlusaPredmetId,
+                        principalTable: "SlusaPredmet",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ispit_AngazovanId",
+                table: "Ispit",
+                column: "AngazovanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IspitStavka_IspitId",
+                table: "IspitStavka",
+                column: "IspitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IspitStavka_SlusaPredmetId",
+                table: "IspitStavka",
+                column: "SlusaPredmetId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IspitStavka");
+
+            migrationBuilder.DropTable(
+                name: "Ispit");
+        }
+    }
+}
