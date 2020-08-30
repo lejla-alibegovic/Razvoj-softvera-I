61c5d56bc169143558f7df298bb11bb04455e86e
Lejla Alibegovic <lejla.alibegovic@edu.fit.ba>
2020-08-30 21:37:08 +0200

    init

---
+++ 2020-07-06/RS1_Ispit_2020_07_06_aspnet_core/RS1_Ispit/EntityModels/Takmicenje.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class Takmicenje
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(PredmetId))]
+        public Predmet Predmet { get; set; }
+        public int PredmetId { get; set; }
+        [ForeignKey(nameof(SkolaId))]
+        public Skola Skola { get; set; }
+        public int SkolaId { get; set; }
+        public DateTime Datum { get; set; }
+        public bool IsZakljucano { get; set; } = false;
+    }
+}
---
+++ 2020-07-06/RS1_Ispit_2020_07_06_aspnet_core/RS1_Ispit/EntityModels/TakmicenjeUcesnik.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class TakmicenjeUcesnik
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(TakmicenjeId))]
+        public Takmicenje Takmicenje { get; set; }
+        public int TakmicenjeId { get; set; }
+        public int? BrojBodova { get; set; }
+        public bool IsPristupio { get; set; }
+        [ForeignKey(nameof(OdjeljenjeStavkaId))]
+        public OdjeljenjeStavka OdjeljenjeStavka { get; set; }
+        public int OdjeljenjeStavkaId { get; set; }
+    }
+}
